6b33db2aba298d3596cb429da8c78722a402f0b5
KaiN <kain@piwnica.ws>
2024-06-20 17:47:49 +0200

    level_compose: read level def file header, auto-match world tiles and tileset

--- src/py_tools/level_compose.py
+++ src/py_tools/level_compose.py
@@ -1,22 +1,40 @@
 from PIL import Image, ImageFont, ImageDraw
+from glob import glob
 import struct
 
 minitile_index_mask_size = 10
 minitile_attribute_bit_flip_y = 5
 minitile_index_mask = (1 << minitile_index_mask_size) - 1
-mini_tiles_path = "../../assets/dec/tileset_world1"
-tile_defs_path = "../../assets/dec/026_tiledef_world1.dat"
-level_tilemap_path = "../../assets/dec/029_level_w1_a0_strt_tiles.dat"
 
-def compose_minitile(tile_image: Image, tiledef, pos):
+def find_path(index: int) -> str:
+    return glob("../../assets/dec/{:03d}*".format(index))[0]
+
+def compose_minitile(tile_image: Image, tileset_path: str, tiledef, pos):
     index = tiledef & minitile_index_mask
     attribute = tiledef >> minitile_index_mask_size
-    minitile = Image.open(f"{mini_tiles_path}/{index}.png")
+    minitile = Image.open(f"{tileset_path}/{index}.png")
     if attribute & (1 << (minitile_attribute_bit_flip_y - 1)) != 0:
         minitile = minitile.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
     tile_image.paste(minitile, pos)
     minitile.close()
 
+
+level_def_path = find_path(305)
+print(f"loading_level {level_def_path}...")
+
+with open(level_def_path, "rb") as file_level_def:
+    file_level_def.seek(31, 0)
+    [level_width] = struct.unpack("<H", file_level_def.read(2))
+    [level_height] = struct.unpack("<H", file_level_def.read(2))
+    file_level_def.seek(36, 0)
+    [level_tilemap_index] = struct.unpack("<H", file_level_def.read(2))
+    [mini_tiles_index] = struct.unpack("<H", file_level_def.read(2))
+    [tile_defs_index] = struct.unpack("<H", file_level_def.read(2))
+
+print(f"dimensions: {level_width}x{level_height}, level tilemap: {level_tilemap_index}, minitiles: {mini_tiles_index}, tiledefs: {tile_defs_index}")
+mini_tiles_path = find_path(mini_tiles_index)
+tile_defs_path = find_path(tile_defs_index)
+level_tilemap_path = find_path(level_tilemap_index)
 defs = []
 with open(tile_defs_path, "rb") as file_mini_tiles:
     while True:
@@ -33,32 +51,33 @@ print(f"tile count: {tile_count}")
 for i in range(tile_count):
     tile_image = Image.new("RGB", [16, 16])
 
-    attribute_a = defs[i][0] >> minitile_index_mask_size
-    attribute_b = defs[i][1] >> minitile_index_mask_size
-    attribute_c = defs[i][2] >> minitile_index_mask_size
-    attribute_d = defs[i][3] >> minitile_index_mask_size
-    print("tile {:02X} attribute {:06b} {:06b} {:06b} {:06b}".format(i, attribute_a, attribute_b, attribute_c, attribute_d))
+    # attribute_a = defs[i][0] >> minitile_index_mask_size
+    # attribute_b = defs[i][1] >> minitile_index_mask_size
+    # attribute_c = defs[i][2] >> minitile_index_mask_size
+    # attribute_d = defs[i][3] >> minitile_index_mask_size
+    # print("tile {:02X} attribute {:06b} {:06b} {:06b} {:06b}".format(i, attribute_a, attribute_b, attribute_c, attribute_d))
 
-    compose_minitile(tile_image, defs[i][0], [0, 0])
-    compose_minitile(tile_image, defs[i][1], [8, 0])
-    compose_minitile(tile_image, defs[i][2], [0, 8])
-    compose_minitile(tile_image, defs[i][3], [8, 8])
+    compose_minitile(tile_image, mini_tiles_path, defs[i][0], [0, 0])
+    compose_minitile(tile_image, mini_tiles_path, defs[i][1], [8, 0])
+    compose_minitile(tile_image, mini_tiles_path, defs[i][2], [0, 8])
+    compose_minitile(tile_image, mini_tiles_path, defs[i][3], [8, 8])
     tiles.append(tile_image)
 
 tile_index_mask = 0b11_1111_1111
-with Image.new("RGBA", [32 * 17, 14 * 17], (0, 0, 64, 255)) as level_preview:
+with Image.new("RGBA", [level_width * 17, level_height * 17], (0, 0, 64, 255)) as level_preview:
     with Image.new("RGBA", level_preview.size, (255, 255, 255, 0)) as txt:
         fnt = ImageFont.truetype("uni05.ttf", 8)
         d = ImageDraw.Draw(txt)
         with open(level_tilemap_path, "rb") as file_tilemap:
-            for y in range(14):
-                for x in range(32):
+            for y in range(level_height):
+                for x in range(level_width):
                     chunk = file_tilemap.read(2)
                     [value] = struct.unpack("<H", chunk)
                     tile_index = value & tile_index_mask
                     tile_attribute = value >> 10
                     level_preview.paste(tiles[tile_index], [x * 17, y * 17])
                     d.text([x * 17, y * 17 - 2], "{:02X}".format(tile_index), font=fnt, fill=(0, 0, 255, 192))
-                    d.text([x * 17, y * 17 - 2 + 8], "{:02X}".format(tile_attribute), font=fnt, fill=(255, 128, 0, 192))
+                    if tile_attribute != 0:
+                        d.text([x * 17, y * 17 - 2 + 8], "{:02X}".format(tile_attribute), font=fnt, fill=(255, 128, 0, 192))
         out = Image.alpha_composite(level_preview, txt)
         out.show()
